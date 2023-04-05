01115309da9b8b8b58655fef55f69e932086ad14
Gabriel Macena <108024946+DevGabrielph@users.noreply.github.com>
2023-04-05 18:03:08 -0300

    a

--- leandro.js
+++ leandro.js
@@ -1,133 +1,54 @@
-//-------Variaveis
+let header=document.querySelector('.header')
 
-var folhaPrincipal= document.querySelector('.listap')
-var folhaItens=document.querySelector('#lista-folha-itens')
-var tituloCabeçalhoItens=document.querySelector('#titulo-cabeçalho-itens')
-var voltar=document.querySelector('#voltar')
-var carrinho=document.querySelector('#carrinho-ext')
+let imgLogo= document.createElement('img')
+imgLogo.src='./imagens-leandro/img-logo/android-chrome-187x66.png'
+header.appendChild(imgLogo)
 
-//------arrays
+let caixaCarrinho=document.createElement('div')
+header.appendChild(caixaCarrinho)
 
-var listaInicial= {
-    promoçoes:document.querySelector('#promoçoes'),
-    porçoes:document.querySelector('#porçoes'),
-    hamburgueres : document.querySelector('#hamb'),
-    cachorroquente : document.querySelector('#cachorro'),
-    bebidas : document.querySelector('#bebidas'),
-    sucos : document.querySelector('#sucos'),
-    vitaminas : document.querySelector('#vitaminas')
+let numberItenscar=document.createElement('p')
+numberItenscar.innerHTML='0'
+caixaCarrinho.appendChild(numberItenscar)
+
+let imgCarrinho= document.createElement('ion-icon')
+imgCarrinho.setAttribute('name','cart-outline')
+caixaCarrinho.appendChild(imgCarrinho)
+imgCarrinho.classList.add('imgCarrinho')
+imgCarrinho.setAttribute('onclick','Carrinho()')
+
+let main=document.querySelector('.main')
+
+
+
+
+function Carrinho(){
+    main.innerHTML=''
+    renderCarrinho()
 
 }
-var tituloItens=['Promoções','Porções','Hambúrgueres','Cachorro Quente','Bebidas','Sucos','Vitaminas']
+function renderCarrinho(){
 
+    let carrinho= document.createElement('div')
+    main.appendChild(carrinho)
 
-//------------ EVENTOSSS--- 
+    let cabecalhoCarrinho= document.createElement('div')
+    carrinho.appendChild(cabecalhoCarrinho)
+    cabecalhoCarrinho.classList.add('voltarCar')
 
-listaInicial.promoçoes.addEventListener('click',promoçoes)
-listaInicial.porçoes.addEventListener('click',porçoes)
-listaInicial.hamburgueres.addEventListener('click',burguer)
-listaInicial.cachorroquente.addEventListener('click',cachorro)
-listaInicial.bebidas.addEventListener('click',bebidas)
-listaInicial.sucos.addEventListener('click',sucos)
-listaInicial.vitaminas.addEventListener('click',vitaminas)
+    let btnVoltarCar=document.createElement('ion-icon')
+    btnVoltarCar.setAttribute('name','return-up-back')
+    btnVoltarCar.setAttribute('onclick','voltarCar()')
+    cabecalhoCarrinho.appendChild(btnVoltarCar)
 
-voltar.addEventListener('click',voltarInicio)
-carrinho.addEventListener('click',interiorCarrinho)
+ 
+    let titulo=document.createElement('p')
+    titulo.innerHTML='Carrinho'
+    cabecalhoCarrinho.appendChild(titulo)
 
-
-
-function promoçoes(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[0]
-
-    var promo =[
-        { nome:'2 X-tudos',descriçao:'<strong>Descrição:</strong> Pão, carne, queijo, presunto, bacon, ovo, alface, ketchup, maionese temperada, e batata palha.',valor:'R$ 19,99',imagem:"imagens/x-tudo.png"},
-
-        { nome:'Combo X-burguer',descriçao:'<strong>Descrição:</strong> lanche + batata frita + guaravita ou guaracamp.',valor:'R$ 12,00',imagem:"imagens/x-tudo.png"},
-
-        { nome:'Combo Duplo cheddar',descriçao:' <strong>Descrição:</strong> lanche + batata frita + guaravita ou guaracamp.',valor:'R$ 16,00',imagem:"imagens/x-tudo.png"},
-
-        { nome:'Combo X-Tudo ',descriçao:'<strong>Descrição:</strong> lanche + batata frita + guaravita ou guaracamp.',valor:'R$ 16,00',imagem:"imagens/x-tudo.png"},
-
-        { nome:'Combo X-Picanha',descriçao:'<strong>Descrição:</strong>  lanche + batata frita + refrigerante lata.',valor:'R$ 25,00',imagem:"imagens/x-tudo.png"},
-
-        { nome:'',descriçao:'',valor:'',imagem:''},
-
-        { nome:'',descriçao:'',valor:'',imagem:''},
-
-        { nome:'',descriçao:'',valor:'',imagem:''},
-
-        { nome:'',descriçao:'',valor:'',imagem:''},
-
-        { nome:'',descriçao:'',valor:'',imagem:''}
-
-    ]
-   
-}
-
-function porçoes(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[1]
-}
-function burguer(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[2]
-}
-function cachorro(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[3]
-}
-function bebidas(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[4]
-}
-function sucos(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[5]
-}
-function vitaminas(){
-    folhaPrincipal.style.display='none'
-    folhaItens.style.display='block'
-    tituloCabeçalhoItens.innerHTML=tituloItens[6]
-}
-
-//------BOTÃO CARRINHO------
-function interiorCarrinho(){
-    let interiorCar ={ 
-        voltarCar:document.querySelector('#voltar-carrinho'),
-        interior:document.querySelector('#interior-carrinho')
-}
     
-    interiorCar.voltarCar.addEventListener('click',voltarCarrinho)
-
-    interiorCar.interior.style.display='block'
-
-    function voltarCarrinho(){
-        interiorCar.interior.style.display='none'
-    
-    }
-    
-
 }
 
-
-
-
-//------------ BOTÃO VOLTAR-------------------
-
-function voltarInicio(){
-
-    folhaPrincipal.style.display='grid'
-    folhaItens.style.display='none'
-
+function voltarCar(){
+    main.innerHTML=''
 }
-
-
-
-
