9fbc1d909bb1901c90973b75cfa6d353e886c1ba
Gabriel Macena <108024946+DevGabrielph@users.noreply.github.com>
2023-04-18 16:18:50 -0300

    a

--- leandro.js
+++ leandro.js
@@ -1,3 +1,34 @@
+let por=[
+    { nome:' Batata Frita 200g',descriçao:'Batata frita , ketchup, maionese temperada,queijo ralado e orégano.',valor:7.00,imagem:'',obs:'',valorF:''},
+    
+    { nome:'Batata Frita 400g',descriçao:'Batata frita , calabresa, ketchup, maionese temperada,queijo ralado e orégano.',valor:14.00,imagem:'',obs:'',valorF:''}
+]
+let Hamb=[
+    { nome:'Hambúrguer',descriçao:' Pão, carne, alface, ketchup ,maionese temperada , e batata palha.',valor:5.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+    
+    { nome:'X-Búrguer',descriçao:'  Pão, carne, queijo, alface, ketchup ,maionese temperada , e batata palha.',valor:6.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+    
+        { nome:'Duplo X-Búrguer ',descriçao:' Pão, 2 carnes, 2 queijos, alface, ketchup ,maionese temperada , e batata palha.',valor:10.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+
+        { nome:'Duplo Cheddar',descriçao:'Pão, 2 carnes, 2 cheddar, alface, ketchup ,maionese temperada , e batata palha.',valor:10.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+        
+        { nome:'X-Egg ',descriçao:' Pão, carne, queijo , alface, ketchup ,maionese temperada , e batata palha.',valor:9.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+
+        { nome:'X-Tudo',descriçao:' Pão, carne, queijo, presunto, bacon, ovo, alface, ketchup ,maionese temperada , e batata palha.',valor:9.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+
+        { nome:'X-Trovão',descriçao:'   Pão, 2 carnes, queijo, presunto, bacon, ovo, alface, ketchup ,maionese temperada , e batata palha.',valor:14.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+
+        { nome:'X-Tudão',descriçao:'  Pão, 2 carnes, 2 queijos, 2 presuntos, bacon, 2 ovos, alface, ketchup ,maionese temperada , e batata palha.',valor:16.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+        
+        { nome:'X-Calabresa',descriçao:'Pão, carne, queijo, presunto, calabresa, ovo, alface, ketchup ,maionese temperada , e batata palha.',valor:13.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''},
+        
+        { nome:'X-Picanha',descriçao:' Pão, carne de picanha 120g, queijo, presunto, bacon, ovo, alface, ketchup ,maionese temperada , e batata palha.',valor:16.00,imagem:'imagens/x-tudo.png',obs:'',valorF:''}
+] 
+
+
+let todosItens= [...por,...Hamb]
+
+console.log(todosItens)
 let header=document.querySelector('.header')
 
 let imgLogo= document.createElement('img')
@@ -29,6 +60,9 @@ function rendePaginaP(){
     main.appendChild(feed)
     
     titulosP.map((todo)=>{
+
+        let posicaoFeed= titulosP.indexOf(todo)
+
         let areaFeed=document.createElement('div')
         areaFeed.classList.add('areafeed')
         feed.appendChild(areaFeed)
@@ -36,6 +70,7 @@ function rendePaginaP(){
         let imgFeed= document.createElement('img')
         imgFeed.src='./imagens-leandro/itens/x-tudo.png'
         areaFeed.appendChild(imgFeed)
+        imgFeed.setAttribute('onclick',`clickTitulos(${posicaoFeed})`)
 
         let titulosFeed= document.createElement('p')
         areaFeed.appendChild(titulosFeed)
@@ -45,7 +80,13 @@ function rendePaginaP(){
         
        
         
-    })   
+    }) 
+    
+    
+}
+
+function clickTitulos(posicaoFeed){
+    alert(posicaoFeed)
 }
 
 function Carrinho(){
